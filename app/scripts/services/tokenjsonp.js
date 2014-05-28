61305890ebac25b8f1ea8f701304f79c9b6b736e
James Jones <jamjon3@gmail.com>
2014-05-28 15:19:38 -0400

    Changing the flow of prerequisite JSONP calls
    This allows the token to be retrieved first and then the call happen
    sequentially

--- app/scripts/services/tokenjsonp.js
+++ app/scripts/services/tokenjsonp.js
@@ -8,38 +8,56 @@ angular.module('tokenExampleApp')
     
     return {
       list: function () {
-        //Creating a deferred object        
-        var deferred = $q.defer();
-        var headers = angular.extend({}, ($rootScope.list.token === undefined || $rootScope.list.token === null)?{}:{'X-Auth-Token': $rootScope.list.token});
-        $log.info({ "listHeaders": headers});
-        // storage.get("list");
-        $http.jsonp('https://dev.it.usf.edu/~james/ExampleApp/services.php',{
-            method  : 'POST',
-            // url     : 'https://dev.it.usf.edu/~james/ExampleApp/services.php',
-            // data    : $.param($scope.formData),  // pass in data as strings
-            // ignoreAuthModule: true,
-            params: { 
-              "service": "list"
-              // ?callback=JSON_CALLBACK
-            },
-            headers: headers
-        }).success(function(data) {
-            //Passing data to deferred's resolve function on successful completion
-            $log.info("I RAN 401 as success");
-            deferred.resolve(data);
-        }).error(function(data, status, headers, config){
-            $log.info("I RAN");
+        var promises = [];
+        if (authService.readyToRetrieveToken('list')) {
+          //code
+          promises.push(authService.requestTokenByService("list").then(function(data) {
             $log.info(data);
-            $log.info(status);
-            
-            // console.log(data);
-            // deferred.resolve(data);
-            //Sending a friendly error message in case of failure
-            deferred.resolve(data);
-            // deferred.reject("An error occured while fetching items");
+            $rootScope.list.token = data.token;
+            return data;
+          },function(errorMessage) {
+            $log.info(errorMessage);
+          }));
+        }
+        //Creating a deferred object
+        return $q.all(promises).then(function(results) {
+          var headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          };
+          var deferred = $q.defer();
+          alert(JSON.stringify($rootScope.list.token));
+          var headers = angular.extend(headers, ($rootScope.list.token === undefined || $rootScope.list.token === null)?{}:{'X-Auth-Token': $rootScope.list.token});
+          $log.info({ "listHeaders": headers});
+          // storage.get("list");
+          $http.jsonp('https://dev.it.usf.edu/~james/ExampleApp/services.php',{
+              method  : 'POST',
+              // url     : 'https://dev.it.usf.edu/~james/ExampleApp/services.php',
+              // data    : $.param($scope.formData),  // pass in data as strings
+              // ignoreAuthModule: true,
+              params: { 
+                "service": "list"
+                // ?callback=JSON_CALLBACK
+              },
+              headers: headers
+          }).success(function(data) {
+              //Passing data to deferred's resolve function on successful completion
+              $log.info("I RAN 401 as success");
+              deferred.resolve(data);
+          }).error(function(data, status, headers, config){
+              $log.info("I RAN");
+              $log.info(data);
+              $log.info(status);
+              
+              // console.log(data);
+              // deferred.resolve(data);
+              //Sending a friendly error message in case of failure
+              deferred.resolve(data);
+              // deferred.reject("An error occured while fetching items");
+          });
+          //Returning the promise object
+          return deferred.promise;
         });
-        //Returning the promise object
-        return deferred.promise;
       }
     };
   }]);
